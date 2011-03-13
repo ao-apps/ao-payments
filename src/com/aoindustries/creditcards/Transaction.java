16d499a406d206e6406520ee71f9e05e3f578b6f
Dan Armstrong <dan@aoindustries.com>
2011-03-13 18:58:14 +0000

    Updated copyright.

--- src/com/aoindustries/creditcards/Transaction.java
+++ src/com/aoindustries/creditcards/Transaction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007-2010 by AO Industries, Inc.,
+ * Copyright 2007-2011 by AO Industries, Inc.,
  * 7262 Bull Pen Cir, Mobile, Alabama, 36695, U.S.A.
  * All rights reserved.
  */
--- src/com/aoindustries/creditcards/usaepay/USAePay.java
+++ src/com/aoindustries/creditcards/usaepay/USAePay.java
@@ -1,7 +1,7 @@
 package com.aoindustries.creditcards.usaepay;
 
 /*
- * Copyright 2008-2010 by AO Industries, Inc.,
+ * Copyright 2008-2011 by AO Industries, Inc.,
  * 7262 Bull Pen Cir, Mobile, Alabama, 36695, U.S.A.
  * All rights reserved.
  */
